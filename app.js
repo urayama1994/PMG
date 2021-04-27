395c388c7c1b261999b60f73c3148f1ac8ffc324
urayama1994 <81061873+urayama1994@users.noreply.github.com>
2021-04-28 01:00:51 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,42 @@ const mysql = require('mysql');
 const app = express();
 
 app.use(express.static('public'));
+// フォームから送信された値を受け取る
+app.use(express.urlencoded({extended: false}));
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'hkzv4gez',
+    database: 'subject_manage'
+  });
+
+connection.connect((err) => {
+    if (err) {
+      console.log('error connecting: ' + err.stack);
+      return;
+    }
+    console.log('success');
+  });
+
+
+
+//ルートURL
 app.get('/',(req,res)=>{
     res.render('index.ejs');
-}
-);
-
-app.post('/',(req,res)=>{
-    res.render('index.ejs')
-
 });
+
+
+ //科目をTableに追加
+app.post('/',(req,res)=>{
+    connection.query(
+        'INSERT INTO items(subject) VALUES(?)',
+        [req.body.addSubject],
+        (error,results) => {
+            res.redirect('/');
+        }
+    );
+});
+
 
 app.listen(3003);
